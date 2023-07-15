ad517c638f9bde8b1ba48a013cd89a5bd4dabc78
Victor Birman <victorbirman@gmail.comn>
2023-07-15 17:41:29 -0300

    cosmetic enhancements

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const audioPlayer = document.createElement("audio");
+let audioDiv = document.querySelector(".radio");
 let dropdown = document.querySelector(".dropdown");
 audioPlayer.src = "https://ipanel.instream.audio:7011/stream";
 
 function playAudio() {
   if (audioPlayer.paused) {
-    audioPlayer.play();
+    let playPromise = audioPlayer.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(x => {
+          audioDiv.innerHTML = "<b>II</b> EN VIVO";
+        })
+        .catch(error => {
+          console.log("audio not loaded");
+        });
+    }
   } else {
     audioPlayer.pause();
+    audioDiv.innerHTML = "&#9654; EN VIVO";
   }
 }
-
 function dropdownToggle() {
   if (dropdown.style.visibility === "hidden") {
     dropdown.style.visibility = "visible";
